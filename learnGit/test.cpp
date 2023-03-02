a089382d65f5b0f1d60b5e725c09ef0612cd81ab
lijiawei <932647261@qq.com>
2023-03-02 17:02:10 +0800

    change

--- learnGit/test.cpp
+++ learnGit/test.cpp
@@ -24,7 +24,26 @@ void test01() {
     }
     // cout<<dp[m - 1][bagWeight]<<endl;
 }
+void test02() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+    int m = weight.size();
+    vector<int> dp(bagWeight + 1, 0);
+    // for(int j = weight[0]; j <= bagWeight; ++j) {
+    //     dp[j] = value[0];
+    // }
+    for(int i = 0; i < m; ++i) {
+        for(int j = bagWeight; j >= weight[i]; --j) {
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+        for(auto & num : dp) cout << num << ' ';cout<<endl;
+    }
+    
+}
 int main() {
-    test01();
+    // test02();
+    vector<int> nums(10, 1);
+    accumulate(nums.begin(), nums.end(), 0);
 
 }
